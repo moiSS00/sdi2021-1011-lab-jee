ee737044e65e57ff94c8652b566267e4b5a8a438
uo270824 <uo270824@uniovi.es>
2021-02-19 18:52:41 +0100

    SDI-2021-1011-3.4-Autenticación y control de acceso

--- sdi2021-1011-spring/src/main/java/com/uniovi/services/InsertSampleDataService.java
+++ sdi2021-1011-spring/src/main/java/com/uniovi/services/InsertSampleDataService.java
@@ -6,25 +6,33 @@ import java.util.Set;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.uniovi.entities.Mark;
 import com.uniovi.entities.User;
 
-//@Service -> Servicio que al crearse, mete datos de prueba 
+@Service
 public class InsertSampleDataService {
-	
+
 	@Autowired
 	private UsersService usersService;
 
 	@PostConstruct
 	public void init() {
-		User user1 = new User("99999990A", "Pedro", "Díaz");
-		User user2 = new User("99999991B", "Lucas", "Núñez");
-		User user3 = new User("99999992C", "María", "Rodríguez");
-		User user4 = new User("99999993D", "Marta", "Almonte");
-		User user5 = new User("99999977E", "Pelayo", "Valdes");
-		User user6 = new User("99999988F", "Edward", "Núñez");
 		
+		User user1 = new User("99999990A", "Pedro", "Díaz");
+		user1.setPassword("123456");
+		User user2 = new User("99999991B", "Lucas", "Núñez");
+		user2.setPassword("123456");
+		User user3 = new User("99999992C", "María", "Rodríguez");
+		user3.setPassword("123456");
+		User user4 = new User("99999993D", "Marta", "Almonte");
+		user4.setPassword("123456");
+		User user5 = new User("99999977E", "Pelayo", "Valdes");
+		user5.setPassword("123456");
+		User user6 = new User("99999988F", "Edward", "Núñez");
+		user6.setPassword("123456");
+
 		Set user1Marks = new HashSet<Mark>() {
 			{
 				add(new Mark("Nota A1", 10.0, user1));
@@ -33,9 +41,9 @@ public class InsertSampleDataService {
 				add(new Mark("Nota A4", 6.5, user1));
 			}
 		};
-		
+
 		user1.setMarks(user1Marks);
-		
+
 		Set user2Marks = new HashSet<Mark>() {
 			{
 				add(new Mark("Nota B1", 5.0, user2));
@@ -44,9 +52,9 @@ public class InsertSampleDataService {
 				add(new Mark("Nota B4", 3.5, user2));
 			}
 		};
-		
+
 		user2.setMarks(user2Marks);
-		
+
 		Set user3Marks = new HashSet<Mark>() {
 			{
 				;
@@ -55,9 +63,9 @@ public class InsertSampleDataService {
 				add(new Mark("Nota C3", 7.0, user3));
 			}
 		};
-		
+
 		user3.setMarks(user3Marks);
-		
+
 		Set user4Marks = new HashSet<Mark>() {
 			{
 				add(new Mark("Nota D1", 10.0, user4));
@@ -65,7 +73,7 @@ public class InsertSampleDataService {
 				add(new Mark("Nota D3", 9.0, user4));
 			}
 		};
-		
+
 		user4.setMarks(user4Marks);
 		usersService.addUser(user1);
 		usersService.addUser(user2);
