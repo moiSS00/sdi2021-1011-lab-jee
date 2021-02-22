c89c0cf9aceee6995571752cb74e752396056b7d
uo270824 <uo270824@uniovi.es>
2021-02-22 14:09:05 +0100

    SDI–1011-sesión3-Ejercicio-complementario3

--- sdi2021-1011-spring/src/main/java/com/uniovi/controllers/MarksController.java
+++ sdi2021-1011-spring/src/main/java/com/uniovi/controllers/MarksController.java
@@ -3,6 +3,8 @@ package com.uniovi.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.uniovi.entities.Mark;
 import com.uniovi.services.MarksService;
 import com.uniovi.services.UsersService;
+import com.uniovi.validators.AddMarkFormValidator;
 
 @Controller
 public class MarksController {
@@ -20,6 +23,9 @@ public class MarksController {
 
 	@Autowired
 	private UsersService usersService;
+	
+	@Autowired
+	private AddMarkFormValidator addMarkValidator;
 
 	@RequestMapping("/mark/list")
 	public String getList(Model model) {
@@ -28,7 +34,12 @@ public class MarksController {
 	}
 
 	@RequestMapping(value = "/mark/add", method = RequestMethod.POST)
-	public String setMark(@ModelAttribute Mark mark) {
+	public String setMark(@Validated Mark mark, BindingResult result,Model model) {
+		addMarkValidator.validate(mark, result);
+		model.addAttribute("usersList",usersService.getUsers()); 
+		if(result.hasErrors()) {
+			return "/mark/add";
+		}
 		marksService.addMark(mark);
 		return "redirect:/mark/list";
 	}
@@ -45,9 +56,10 @@ public class MarksController {
 		return "redirect:/mark/list";
 	}
 
-	@RequestMapping(value = "/mark/add")
+	@RequestMapping(value = "/mark/add", method = RequestMethod.GET)
 	public String getMark(Model model) {
 		model.addAttribute("usersList", usersService.getUsers());
+		model.addAttribute("mark",new Mark());
 		return "mark/add";
 	}
 
---
+++ sdi2021-1011-spring/src/main/java/com/uniovi/validators/AddMarkFormValidator.java
@@ -0,0 +1,31 @@
+package com.uniovi.validators;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.uniovi.entities.Mark;
+
+@Component
+public class AddMarkFormValidator implements Validator {
+	
+	@Override
+	public boolean supports(Class<?> aClass) {
+		return Mark.class.equals(aClass);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		Mark mark = (Mark) target; 
+		
+		if (mark.getScore() < 0 || mark.getScore() > 10) {
+			errors.rejectValue("score", "Error.addMark.score.value");
+		}
+		
+		if (mark.getDescription().length() < 20) {
+			errors.rejectValue("description", "Error.addMark.description.length");
+		}
+		
+	}
+
+}
