e2be1f78750453c3137d63d42bb59c9f798f2dde
uo270824 <uo270824@uniovi.es>
2021-03-04 21:10:11 +0100

    SDI-2021-1011-5.4-Casos Uso VistaHome

--- sdi2021-1011-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ sdi2021-1011-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -5,6 +5,10 @@ import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_Properties;
+import com.uniovi.tests.util.SeleniumUtils;
+
 import org.junit.runners.MethodSorters;
 
 //Ordenamos las pruebas por el nombre del método
@@ -57,8 +61,32 @@ public class NotaneitorTests {
 		driver.quit();
 	}
 
+	// PR01. Acceder a la página principal /
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void PR01() {
+		PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH());
 	}
+
+	// PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
+	@Test
+	public void PR02() {
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+	}
+
+	// PR03. OPción de navegación. Pinchar en el enlace Identificate en la página
+	// home
+	@Test
+	public void PR03() {
+		PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	}
+
+	// PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a
+	// Español
+	@Test
+	public void PR04() {
+		PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(),
+				PO_Properties.getENGLISH());
+		SeleniumUtils.esperarSegundos(driver, 2);
+	}
+
 }
